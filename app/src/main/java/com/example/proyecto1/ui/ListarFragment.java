045982b4f3aaa04c97dc3b4c5c801a1a9136cf6c
JessMtzP <75195715+JessMtzP@users.noreply.github.com>
2020-12-01 16:38:51 -0600

    Add files via upload

---
+++ app/src/main/java/com/example/proyecto1/ui/ListarFragment.java
@@ -0,0 +1,39 @@
+package com.example.proyecto1.ui.listar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.proyecto1.R;
+
+public class ListarFragment extends Fragment {
+
+    private ListarViewModel listarViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        listarViewModel =
+                new ViewModelProvider(this).get(ListarViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_listar, container, false);
+        listarViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+
+            }
+        });
+        return root;
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getActivity().getMenuInflater().inflate(R.menu.menu_admin,menu);
+        return true;
+    }
+}
