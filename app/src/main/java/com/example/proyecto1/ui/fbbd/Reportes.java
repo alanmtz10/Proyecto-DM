3f5cd64531bb0fcf8b81f853c754e1b6a8cf4a44
26Alondra <asolisr1@toluca.tecnm.mx>
2020-12-29 12:17:46 -0600

    Avance reporte

---
+++ app/src/main/java/com/example/proyecto1/ui/fbbd/Reportes.java
@@ -0,0 +1,54 @@
+package com.example.proyecto1.ui.fbbd;
+
+public class Reportes {
+
+    private String anomalia;
+    private String descripcion;
+    private String ubicacion;
+    private String fecha;
+    private String usuario;
+
+    public void Reportes(){
+
+    }
+
+    public String getAnomalia() {
+        return anomalia;
+    }
+
+    public void setAnomalia(String anomalia) {
+        this.anomalia = anomalia;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+}
--- app/src/main/java/com/example/proyecto1/ui/reporte/ReporteFragment.java
+++ app/src/main/java/com/example/proyecto1/ui/reporte/ReporteFragment.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,9 +18,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.proyecto1.R;
 
-public class ReporteFragment extends Fragment {
+public class ReporteFragment extends Fragment implements  AdapterView.OnItemSelectedListener, View.OnClickListener {
 
     private ReporteViewModel reporteViewModel;
+
+    private Spinner spAnomalia;
+
+    private EditText descripcion,ubicacion, fecha;
+
+    private Button aceptar, cancelar;
+
+    private String a;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,6 +41,55 @@ public class ReporteFragment extends Fragment {
 
             }
         });
+        iniciaComponentes(root);
+        spinnerComponents(root);
+
         return root;
+    }
+
+    private void iniciaComponentes(View root){
+        descripcion = root.findViewById(R.id.eTDescripcionC);
+        ubicacion = root.findViewById(R.id.eTUbicacionC);
+        fecha = root.findViewById(R.id.eTFechaC);
+        aceptar = root.findViewById(R.id.btnAceptarRep);
+        cancelar = root.findViewById(R.id.btnCerrarRep);
+    }
+
+    private void spinnerComponents(View root){
+        ArrayAdapter<CharSequence> anomaliaAdapter;
+        anomaliaAdapter = ArrayAdapter.createFromResource(getContext(), R.array.opciones, android.R.layout.simple_spinner_item);
+
+        spAnomalia = (Spinner) root.findViewById(R.id.spnAnomaliaC);
+        spAnomalia.setAdapter(anomaliaAdapter);
+        spAnomalia.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (parent.getId()){
+            case R.id.spnAnomaliaC:
+                if (position != 0){
+                    a = parent.getItemAtPosition(position).toString();
+                } else {
+                    a = "";
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnAceptarRep:
+                break;
+            case R.id.btnCerrarRep:
+                break;
+
+        }
     }
 }
