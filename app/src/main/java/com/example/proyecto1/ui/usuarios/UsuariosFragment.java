1a117ccf6d1beb4a3462ed2bfe688934960b3b31
Lucano <lpriorg@toluca.tecnm.mx>
2021-01-02 17:34:53 -0600

    Lista Usuarios al 100 por ahora

--- app/src/main/java/com/example/proyecto1/ui/usuarios/UsuariosFragment.java
+++ app/src/main/java/com/example/proyecto1/ui/usuarios/UsuariosFragment.java
@@ -26,6 +26,10 @@ import com.google.android.gms.dynamic.IFragmentWrapper;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -78,7 +82,7 @@ public class UsuariosFragment extends Fragment {
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference();
         firebaseAuth = FirebaseAuth.getInstance();
-        user = FirebaseAuth.getInstance().getCurrentUser();
+
 
         listarDatos();
 
@@ -106,18 +110,19 @@ public class UsuariosFragment extends Fragment {
                 dialogo.setTitle("Usuario");
                 dialogo.setView(dialogView);
                 dialogo.setNeutralButton("Aceptar", null);
+                Usuarios u = new Usuarios();
+                u.setId(usuarioSelected.getId());
+                u.setNombre(usuarioSelected.getNombre());
+                u.setUsuario(usuarioSelected.getUsuario());
+                u.setCorreo(usuarioSelected.getCorreo());
+                u.setContrasenia(usuarioSelected.getContrasenia());
+                u.setDireccion(usuarioSelected.getDireccion());
+                u.setEdad(usuarioSelected.getEdad());
+                u.setTelefono(usuarioSelected.getTelefono());
                 dialogo.setPositiveButton("Cambiar Rango", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Usuarios u = new Usuarios();
-                        u.setId(usuarioSelected.getId());
-                        u.setNombre(usuarioSelected.getNombre());
-                        u.setUsuario(usuarioSelected.getUsuario());
-                        u.setCorreo(usuarioSelected.getCorreo());
-                        u.setContrasenia(usuarioSelected.getContrasenia());
-                        u.setDireccion(usuarioSelected.getDireccion());
-                        u.setEdad(usuarioSelected.getEdad());
-                        u.setTelefono(usuarioSelected.getTelefono());
+
                         if (usuarioSelected.getTipo().toUpperCase().equals("USUARIO")){
                             u.setTipo("Administrador");
                         }
@@ -132,20 +137,61 @@ public class UsuariosFragment extends Fragment {
                 dialogo.setNegativeButton("Eliminar Usuario", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Usuarios u = new Usuarios();
-                        u.setId(usuarioSelected.getId());
-                        databaseReference.child("Usuarios").child(u.getId()).removeValue();
-
-                        user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        user=firebaseAuth.getCurrentUser();
+                        //databaseReference.child("Usuarios").child(u.getId()).removeValue();
+                        databaseReference.child("Usuarios").child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
                             @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                Toast.makeText(getContext(),"Usuario Eliminado",Toast.LENGTH_LONG).show();
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                Usuarios sesion=snapshot.getValue(Usuarios.class);
+                                firebaseAuth.signOut();
+                                firebaseAuth.signInWithEmailAndPassword(u.getCorreo(),u.getContrasenia())
+                                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()){
+                                            FirebaseUser user2=FirebaseAuth.getInstance().getCurrentUser();
+                                            AuthCredential credencial=EmailAuthProvider.getCredential(u.getCorreo(),u.getContrasenia());
+                                            user2.reauthenticate(credencial).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    Toast.makeText(getContext(),"Correo 1:"+sesion.getCorreo()+" Correo 2:"+user2.getEmail(),Toast.LENGTH_LONG).show();
+                                                    user2.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                            if(task.isSuccessful()){
+                                                                Toast.makeText(getContext(),"El usuario se eliminó correctamente",Toast.LENGTH_LONG).show();
+                                                            }else{
+                                                                Toast.makeText(getContext(),"Error al eliminar el usuario",Toast.LENGTH_LONG).show();
+                                                                firebaseAuth.signOut();
+                                                            }
+                                                            firebaseAuth.signInWithEmailAndPassword(sesion.getCorreo(),sesion.getContrasenia())
+                                                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                                                if (task.isSuccessful()){
+                                                                }
+                                                            }
+                                                        });
+                                                        }
+                                                    });
+
+                                                }
+                                            });
+                                        }
+                                    }
+                                });
+
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
                             }
                         });
                     }
                 });
                 dialogo.show();
-
             }
         });
 
